67d288182d8c8f3d5e7e73b83276d8c945d6addd
Tugba <tgbaozkn1995@gmail.com>
2022-03-26 23:55:14 +0300

    updating board

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useState } from 'react';
-import { createGlobalStyle } from 'styled-components';
+
 
 
 export default function Board(props) {
