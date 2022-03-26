e1b39604b30723d489d6017e58d42ef322980dba
Tugba <tgbaozkn1995@gmail.com>
2022-03-26 23:56:03 +0300

    updating game page

--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Board from '../Components/Board'
 import '../App.css';
-import Square from '../Components/Square';
+
 export default function Game() {
   return (
     <div>
